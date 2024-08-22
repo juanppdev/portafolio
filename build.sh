323913267c28613174c40e77b4b816e9cf424de7
Juan Pablo Patino Lopez <fomundi.fr@gmail.com>
2024-08-22 20:41:38 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf public
 reflex init
 reflex export --frontend-only
 unzip frontend.zip -d public
