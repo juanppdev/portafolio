aa804bdf961dd33c2d16d1635e4027cc366f6b8d
Juan Pablo Patino Lopez <juanppdev@gmail.com>
2024-10-20 00:54:18 +0200

    Update remote_build.sh

--- remote_build.sh
+++ remote_build.sh
@@ -1,8 +1,7 @@
-python -m venv .venv
 source .venv/bin/activate
+rm -rf public
 pip install --upgrade pip
 pip install -r requirements.txt
-rm -rf public
 reflex init
 reflex export --frontend-only
 unzip frontend.zip -d public
